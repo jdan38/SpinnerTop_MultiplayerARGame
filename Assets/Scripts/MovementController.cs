39cab33a5a8898950b7249e12289c45e056359df
jdan38 <jdev0016@gmail.com>
2021-01-31 03:35:29 -0500

    Add project files.

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public Joystick joystick;
+    public float speed = 2f;
+    private Vector3 velocityVector = Vector3.zero;//initial velocity 
+
+    public float maxvelocityChange = 4f;
+    private Rigidbody rb;
+
+
+    public float tiltAmount = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Taking the joystick inputs
+        float _xMovementInput = joystick.Horizontal;
+        float _zMovementInput = joystick.Vertical;
+
+        //Calculating velocity vectors
+        Vector3 _movementHorizontal = transform.right * _xMovementInput;
+        Vector3 _movementVertical = transform.forward * _zMovementInput;
+
+        //Calculate the final movement velocity vector
+        Vector3 _movementVelocityVector = (_movementHorizontal + _movementVertical).normalized * speed;
+
+        //Apply Movement
+        Move(_movementVelocityVector);
+
+        transform.rotation = Quaternion.Euler(joystick.Vertical * speed * tiltAmount, 0, -1 * joystick.Horizontal * speed * tiltAmount);
+    }
+
+    void Move(Vector3 movementVelocityVector)
+    {
+        velocityVector = movementVelocityVector;
+    }
+
+    private void FixedUpdate()
+    {
+        if (velocityVector != Vector3.zero)
+        {
+            //Get rigidbody's current velocity
+            Vector3 velocity = rb.velocity;
+            Vector3 velocityChange = (velocityVector - velocity);
+
+            //Apply a force by the amount of velecity change to reach the target velcity
+            velocityChange.x = Mathf.Clamp(velocityChange.x,-maxvelocityChange,maxvelocityChange );
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxvelocityChange, maxvelocityChange);
+            velocityChange.y = 0f;
+
+
+            rb.AddForce(velocityChange, ForceMode.Acceleration);
+        }
+
+        
+    }
+}
